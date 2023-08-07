2ffb4878c0836cd10afa01ea5ae379258fe5a77b
Selim Ocak <selimocakbe@gmail.com>
2023-08-07 17:38:34 +0300

    js-giris

--- 010-JavaScript/js/003-variable.js
+++ 010-JavaScript/js/003-variable.js
@@ -1,22 +1,11 @@
-let API_KEY = "OSDIFHSDSDFGISDHGOSDIHGOIHDSFGOIHREOIG4398UY3498UERFGE98YTG4IEHRFIERHYG9834Y";
+let API_KEY = "KSDOPFMDHGJFHGDHGDHFGNDFNGJKDFDGFKGDFHGUIFDGHUFUSFUDFGHJFGFHGFG";
 
 console.log(API_KEY)
 
 // firstName -> camelCase
-// FirstName ???
-
 
 // Degisken Isimlerinin Dogru Kullanilmasi
-let firstName = "Dogu"
-let FirstName = "JSte Kabul Gormuyor"
-let FIRST_NAME = "Bu Degisken Bilgisinin Degismeyeceyi Varsayilir.."
-FIRST_NAME = "Bilgi let ile tanimlandigi icin degistirildi"
+let firstName = "Dogru"
+let FirstName = "JS de kabul gormuyor"
+let FIRST_NAME = "Bu degisken bilgisinin degismeyecegi varsayilir"
 console.log(firstName)
-
-// Neden Ingilizce Degisken Ismi Kullaniyoruz?
-let adım = "Hakan :)" // Bunu Kullanmayin..
-console.log(adım)
-
-// Kotu Degisken Ismi Tanimlamayi Sevenler Icin Ornek :)
-let x = "Yalcinkaya"
-console.log(x)
