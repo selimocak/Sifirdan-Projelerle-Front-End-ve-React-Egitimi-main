d3eef31a9ffc2b5bc909b331f57459b8fa55656d
Selim Ocak <selimocakbe@gmail.com>
2023-08-08 14:09:47 +0300

    js-1

---
+++ 010-JavaScript/denemeler/console.js
@@ -0,0 +1,16 @@
+console.count("count ?")
+console.log(console)
+console.count("count ?")
+console.error("Hata bilgisini dikkat cekmesi icin error ile yazabiliriz")
+
+const user = {
+    firstName: "Selim",
+    lastName: "Selim",
+}
+
+console.log("log:", user)
+
+console.log("TABLE::::")
+console.table(user)
+
+console.info("Bilgi...")
--- 010-JavaScript/js/003-variable.js
+++ 010-JavaScript/js/003-variable.js
@@ -8,4 +8,13 @@ console.log(API_KEY)
 let firstName = "Dogru"
 let FirstName = "JS de kabul gormuyor"
 let FIRST_NAME = "Bu degisken bilgisinin degismeyecegi varsayilir"
+FIRST_NAME = "Bilgi let ile tanimlandigi icin degistirildi"
 console.log(firstName)
+
+let adım = "Selim" // Bunu kullanmayin
+console.log(adım)
+
+
+// Kotu degisken ismi tanimlama
+let x = "asd"
+console.log(x)
