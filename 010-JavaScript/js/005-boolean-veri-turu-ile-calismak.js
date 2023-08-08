60e1018279e0182582fa92376fed1a0667fb65f6
Selim Ocak <selimocakbe@gmail.com>
2023-08-08 17:20:02 +0300

    booleon-data

--- 010-JavaScript/js/005-boolean-veri-turu-ile-calismak.js
+++ 010-JavaScript/js/005-boolean-veri-turu-ile-calismak.js
@@ -1,8 +1,8 @@
-// degisken isimlerinin is, has ile baslatilmasi avantaj saglar..
-let isUserActive = false // 0
+// degisken isimlerinin is, has ile baslatilmasi avantaj saglar.
+let isUserActive = false // 0 
 
-// kullanici adi ve sifresi soruldu ve dogrulandi :)
-isUserActive = true // 1
+// kullanici adi ve sifresi soruldu ve dogrulandi
+isUserActive = true
 console.log(isUserActive)
 
 let user;
@@ -13,17 +13,5 @@ console.log(Boolean("")) // false
 console.log(Boolean("admin")) // true
 console.log(Boolean(1)) // true
 console.log(Boolean(0)) // false
-console.log("0.1", Boolean(0.1))
-console.log("1 > 3", 1 > 3) // false
-
-console.log(
-  Boolean(-0)
-)
-
-console.log(Boolean(0 === 0))
-
-user = "loremIpsum"
-
-// IF Yapisinda Tekrar Isleyecegiz
-console.log("Boolean(user)", Boolean(user))
-console.log("Boolean(user.length > 9)", Boolean(user.length > 9))
+console.log("0.1", Boolean(0.1)) // true
+console.log(1 > 3) // false
