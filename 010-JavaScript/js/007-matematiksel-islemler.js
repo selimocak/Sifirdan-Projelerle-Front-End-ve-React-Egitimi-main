e48817004bb29a982a8405c116a91b8185ad7547
Selim Ocak <selimocakbe@gmail.com>
2023-08-09 16:06:18 +0300

    matematiksel-islemler

--- 010-JavaScript/js/007-matematiksel-islemler.js
+++ 010-JavaScript/js/007-matematiksel-islemler.js
@@ -1,30 +1,26 @@
 let userId = 37;
+
 // mod alma:
 console.log(userId / 2)
-console.log("tek mi cift mi?", userId % 2)
+console.log("tek mi cift mi ?", userId % 2)
 
 userId ++;
-console.log("tek mi cift mi?", userId % 2)
+console.log("tek mi cift mi ?", userId % 2)
 
 const boxSize = 8;
 let items = 29;
 
-console.log("Son Kolide kac urun var:", items % boxSize)
-console.log("Son Koliye kac adet urun daha eklenmeli?", boxSize - (items % boxSize) )
+console.log("son box icinde kac urun var:", items % boxSize)
+console.log("son box a kac oge daha eklenmeli? ", boxSize - (items % boxSize))
 
-// us alma
+// us alma:
 console.log(3 * 3 * 3 * 3)
 console.log(3 ** 4)
 
-// Asagi Yuvarlama
-console.log("Asagi Yuvarlama", Math.floor(3.9) )
+// asagi yuvarlama:
+console.log("Asagi yuvarlama", Math.floor(3.9))
 
-// Yukari Yuvarlama
-console.log("Yukari Yuvarlama", Math.ceil(3.9) )
+// yukari yuvarlama:
 
-// Yakina Yuvarlama
-console.log("Yakina Yuvarlama 3.9",  Math.round(3.9) )
-console.log("Yakina Yuvarlama 3.6",  Math.round(3.6) )
-console.log("Yakina Yuvarlama 3.5",  Math.round(3.5) )
-console.log("Yakina Yuvarlama 3.49", Math.round(3.49) )
-console.log("Yakina Yuvarlama 3.4",  Math.round(3.4) )
+
+// yakina yuvarlama:
