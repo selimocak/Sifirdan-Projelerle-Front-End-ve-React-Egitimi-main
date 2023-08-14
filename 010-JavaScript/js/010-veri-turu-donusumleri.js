51eb5ac2fd78f0cc55b387e55b23c937ca084c69
Selim Ocak <selimocakbe@gmail.com>
2023-08-14 14:42:01 +0300

    veri_turu-typeof

--- 010-JavaScript/js/010-veri-turu-donusumleri.js
+++ 010-JavaScript/js/010-veri-turu-donusumleri.js
@@ -1,24 +1,23 @@
-let isActive = true
-let firstName = "Beyza"
+let isActive = true 
+let firstName = "Ali"
 let year = 2023
 
 console.log("isActive", isActive, typeof(isActive))
-console.log("firstName", firstName, typeof(firstName))
-console.log("year", year, typeof(year))
-console.log("year bilgisi number mi ?", typeof(year) === "number")
+console.log("firstName", firstName, typeof(firstName))  
+console.log("year", year, typeof(year))  
+console.log("year bilgisi number mi ? ", typeof(year) === "number")  
 
 let age = 35;
 let addToAge = "11.37abc"
 let result = age + addToAge
 
-console.log("age + addToAge : ", result)
 console.log("typeof age : ", typeof(age))
 console.log("typeof addToAge : ", typeof(addToAge))
-console.log("typeof result : ", typeof(result))
+console.log("typeof result : ", typeof(result)) 
 
 // string icindeki bilgiyi parse etmeye calisiyoruz
-console.log(typeof(addToAge), typeof(parseInt(addToAge)))
-console.log(addToAge, parseInt(addToAge))
+console.log(typeof(addToAge), typeof(parseInt(addToAge))) 
+console.log(addToAge, parseInt(addToAge)) 
 
 result = age + parseInt(addToAge)
 console.log("age + addToAge : ", result)
@@ -27,9 +26,10 @@ console.log("age + addToAge : ", result)
 result = age + Number(addToAge)
 console.log("age + addToAge : ", result)
 
+
 result = age + parseFloat(addToAge)
 console.log("age + addToAge : ", result)
 
 let strResult = result.toString()
-console.log("strResult ", strResult)
+console.log("strResult", strResult)
 console.log("strResult typeof", typeof(strResult))
