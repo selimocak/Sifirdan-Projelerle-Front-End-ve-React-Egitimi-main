b1d9420f433e285c4c68c372cfb5c60fd1641d6b
Selim Ocak <selimocakbe@gmail.com>
2023-08-15 12:10:03 +0300

    JSDoc-support

--- 010-JavaScript/js/014-aciklama-satirlari-ile-calismak.js
+++ 010-JavaScript/js/014-aciklama-satirlari-ile-calismak.js
@@ -2,17 +2,18 @@
 // https://code.visualstudio.com/docs/languages/javascript#_jsdoc-support
 // https://jsdoc.app/
 
-console.log("Info")  // console.log'a aktarilan bilgi
+console.log("info")  // console.log`a aktarilan bilgi 
 
-/*
+/* 
 birden fazla satirli aciklama
-ikinci satirda farkli bir aciklama daha var :)
+ikinci satir
+ucuncu...
 */
 
 // JSDoc Ornegi
 /**
- * Fonksiyonun Aciklama Bilgisi..
- * @param {number} num1  // parametreler..
+ * 
+ * @param {number} num1 
  * @param {number} num2 
  * @returns 
  */
@@ -21,13 +22,13 @@ function sum(num1, num2) {
 }
 
 /**
- * Add String to List
+ * Add string to list
  * @param {Array} users
  * @param {string} user
  * @returns
  */
 function addToList(users, user) {
-  console.log(users.length) // console log a ilk gelen users icinde kac oge oldugunu bildirir..
+  console.log(users.length)  // consol.log'a ilk gelen users icinde kac oge oldugunu bildirir.
   users.push(user)
   return users
 }
