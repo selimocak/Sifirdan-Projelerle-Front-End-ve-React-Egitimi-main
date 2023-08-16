26a00c0655a7d93c384bc735c3e76b50822079a4
Selim Ocak <selimocakbe@gmail.com>
2023-08-16 16:16:11 +0300

    coklu_kosullar-if-else

--- 010-JavaScript/js/018-coklu-kosullar-ile-calismak.js
+++ 010-JavaScript/js/018-coklu-kosullar-ile-calismak.js
@@ -1,41 +1,38 @@
 const userId = prompt("TC No Giriniz: ")
 const userAge = prompt("Yasinizi Giriniz: ")
 
-// not: If her zaman true'da calisir..
-// if (true) {console.log('calisti..')}
-// if (false) {console.log('calismaz')}
-// if (!false) {console.log('calisir')}
+// not : if her zaman true'da calisir.
+/**
+ * if (true) {console.log('calisti..')}
+ * if (false) {console.log('calismaz..')}
+ */
 
-// TC length 11 olacak
-// 12345678900
+// TC length 11 olacak 
 
-// negatiften pozitife..
-// if (userId.length !== 11) { 
+// negatiften pozitife,
+// if (userId.length !== 11) {
 //   console.log("TC No Bilgisi Dogru Olmayabilir..")
 // } else if (userId.length === 11 && userAge < 18) {
-//   console.log("Siteye Giremezsiniz Cunku 18 Yasindan Kucuksun..")
+//   console.log("Siteye giremezsiniz 18 yasindan kucuksunuz.")
 // } else if (userId.length === 11 && userAge >= 18) {
-//   console.log("Siteye Girebilirsiniz..")
-// }
+//   console.log("Sisteme girebilirsiniz.")
+// } 
 
-// pozitiften negatife..
-
+// pozitiften negatife,
 if (userId.length === 11 && userAge >= 18 && userAge <= 99) {
-  const h1TitleElement = document.querySelector('h1#title')
-  h1TitleElement.innerHTML = '<em>Siteye Girebilirsiniz..</em>'
-  // h1TitleElement.innerText = '<em>Siteye Girebilirsiniz..</em>'
   // if (userAge > 99) {
-  //   console.log("Yas Bilgisi Hatali..") // acaba burda olmasi dogr muydu ?
+  //   console.log("Yas Bilgisi Hatali.")  // burada olmasi dogru mu ?
   // }
-  // console.log("Siteye Girebilirsiniz..")
-} else if (!userAge <= 99) { 
-  console.log("Yas Bilgisi 99dan buyuk olabilir..")
-  // tersini al operatorunu kullandik ;)
-} else if (userAge > 99) { 
-  console.log("Yas Bilgisi 99dan buyuk olabilir..")
-} else if (userId.length !== 11) { 
+  console.log("Sisteme girebilirsiniz.")
+} else if (!userAge <= 99) {
+  console.log("Yas Bilgisi 99'dan buyuk olabilir.")
+  // tersini al operatorunu kullandik.
+} else if (userAge > 99) {
+  console.log("Yas Bilgisi 99'dan buyuk olabilir.")
+}
+  else if (userId.length !== 11) {
   console.log("TC No Bilgisi Dogru Olmayabilir..")
 }
-else if (userId.length === 11 && userAge < 18) {
-  console.log("Siteye Giremezsiniz Cunku 18 Yasindan Kucuksun..")
-}
+  else if (userId.length === 11 && userAge < 18) {
+  console.log("Siteye giremezsiniz cunku 18 yasindan kucuksunuz.")
+} 
