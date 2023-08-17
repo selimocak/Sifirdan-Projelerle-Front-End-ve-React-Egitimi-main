d030dd8fde7180ecf4e7004a205560109681c2b6
Selim Ocak <selimocakbe@gmail.com>
2023-08-17 15:35:59 +0300

    array-1

--- 010-JavaScript/js/021-Array-ile-Calismak.js
+++ 010-JavaScript/js/021-Array-ile-Calismak.js
@@ -1,53 +1,54 @@
 // Diziler -> Array
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 
-const users = [999, 2999, 3999, false, true, null, [4, 5, 6], "Deniz", "Derya"]
+const users = [1, 2, 3, false, true, null, [4, 5, 6], "string1", "string2"]
 
-console.log(users)
+console.log("users :", users)
 console.log("users.length", users.length)
+
 
 const otherUsers = []
 console.log("otherUsers.length", otherUsers.length)
 
 const tech = new Array(100)
 console.log("tech.length", tech.length)
-console.log("tech Arr:", tech)
+console.log("tech Arr:", tech) 
 
 const todo = new Array("item 1", "item 2")
-console.log("todo", todo)
+console.log("todo", todo) 
 
-console.log("Type Bilgisi Nedir?", typeof(users))
-console.log("{} Obj -> Type Bilgisi Nedir?", typeof( {} ))
+console.log("Type Bilgisi Nedir ? ", typeof(users))     
+console.log("{} Obj -> Type Bilgisi Nedir ? ", typeof( {} ))
 
-console.log("Array misin :) ", Array.isArray(users))
+console.log("Array mi ? ", Array.isArray(users)) 
 
-// Bilgiye Erismek:
-console.log( users[0], users[3], users[7] )
 
-console.log( users[users.length -1] ) // bu cok onemli cunku array'deki son ogeye erismek her zaman onemlidir..
-console.log( users[users.length -2] )
-console.log( users[6][1] )
+// Bilgiye Erismek;
+console.log(users[0], users[3], users[7]) 
+console.log(users[users.length -1]) // Array'daki son ogeye erismek icin bu cok onemli.
+console.log(users[users.length -2]) // Array'da sondan bir onceki ogeye erisir.
 
 
 // Yeni Oge Eklemek:
-// Sona Eklemek:
-users.push("Lorem")
-console.log( users )
-// Basa Eklemek:
-users.unshift("Ilk Siraya Eklenen Oge")
-console.log( users )
+// Sona Ekleme; 
+users.push("Sona")
+console.log(users)
+// Basa Ekleme;
+users.unshift("Basa")
+console.log(users)
 
-// Bilgiyi Degistirmek:
-users[3] = "w4w354" 
-console.log( users )
+// Bilgi Degistirmek;
+users[3] = "asdasd"
+console.log(users)
 
-// Bir Ogeyi Silmek:
-// sondaki
-const popItem = users.pop() // son ogeyi cikarir..
-console.log( users )
-console.log( popItem )
+// Bir Ogeyi Silmek;
+// sondaki 
+const popItem = users.pop()  // Son Ogeyi Cikarir..
+console.log(users)
+console.log(popItem)
 
-// bastaki:
-const firstItem = users.shift()
-console.log( users )
-console.log( firstItem )
+// bastaki
+const firstItem = users.shift()  // Ilk Ogeyi Cikarir..
+console.log(users)
+console.log(firstItem) 
+
