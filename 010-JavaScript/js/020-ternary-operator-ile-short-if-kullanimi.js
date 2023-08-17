8ac1055b9a4593b06f4a2e06fa2afdca45b1e1cf
Selim Ocak <selimocakbe@gmail.com>
2023-08-17 11:33:07 +0300

    ternary-operator

--- 010-JavaScript/js/020-ternary-operator-ile-short-if-kullanimi.js
+++ 010-JavaScript/js/020-ternary-operator-ile-short-if-kullanimi.js
@@ -1,19 +1,17 @@
-const userName = "Leyla Utku"
+const userName = "Ali Mehmet"
 const age = 19;
 const h1TitleElement = document.querySelector('#title')
 
 // if (age >= 18) {
-//   h1TitleElement.innerHTML = `Sitemize Hosgeldin ${userName}`
+//     h1TitleElement.innerHTML = `Sitemize Hosgeldin, ${userName}`
 // } else {
-//   h1TitleElement.innerHTML = `Sitemize Giris Yapamazsin, Uzgunum ${userName}`
+//     h1TitleElement.innerHTML = `Sitemize Giris Yapamazsin, ${userName}`
 // }
 
-
-// kosul ? dogruysa : yanlissa
-// const info = age >= 18 ? `Sitemize Hosgeldin ${userName}` : `Sitemize Giris Yapamazsin, Uzgunum ${userName}`
+// kosul ? dogruysa, : yanlissa
+// const info = age >= 18 ? `Sitemize Hosgeldin, ${userName}` : `Sitemize Giris Yapamazsin, ${userName}` 
 
 // h1TitleElement.innerHTML = `${info}`
 // h1TitleElement.innerHTML = info
-h1TitleElement.innerHTML = `${age >= 18 ? 'Sitemize Hosgeldin ' + userName : 'Sitemize Giris Yapamazsin, Uzgunum ' + userName}`
-
-// h1TitleElement.innerHTML = `${age >= 18 && 'Sitemize Hosgeldin ' + userName}`
+// h1TitleElement.innerHTML = `${age >= 18 && 'Sitemize Hosgeldin, ' + userName}`
+h1TitleElement.innerHTML = `${age >= 18 ? 'Sitemize Hosgeldin, ' + userName : 'Sitemize Giris Yapamazsin,' + userName}` 
