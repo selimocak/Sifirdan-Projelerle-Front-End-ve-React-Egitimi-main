8bba42e4f74431743f30944d7e6676c97b95e597
Selim Ocak <selimocakbe@gmail.com>
2023-08-18 11:46:44 +0300

    function-1

--- 010-JavaScript/js/022-Array-Metodlari-ile-Calismak.js
+++ 010-JavaScript/js/022-Array-Metodlari-ile-Calismak.js
@@ -2,7 +2,7 @@
 // Array Metodlari:
 // https://www.w3schools.com/js/js_array_methods.asp
 
-const femaleUsers = ["Melda", "Kayra", "Ceren"]
+const femaleUsers = ["Melda", "Ayla", "Ceren"]
 const maleUsers = ["Metin", "Kemal", "Can"]
 const items = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 const mixItems = [1, 2, 200, 100, 1000, 99, 3, 4, 5, 6, 7, 8, 9]
@@ -22,17 +22,55 @@ console.log(items)
 console.log(items.slice(1, 5)) 
 
 // includes
-console.log("Kayra var mi ? ", femaleUsers.includes("Kayra"))
+console.log("Ayla var mi ? ", femaleUsers.includes("Ayla"))
 
 // indexOf
-console.log("Kayra nerede ? ", femaleUsers.indexOf("Kayra"))
-console.log("Ayla nerede ? ", femaleUsers.indexOf("Ayla"))  // -1 degeri geldi, yani yok demek.
+console.log("Ayla nerede ? ", femaleUsers.indexOf("Ayla"))
+console.log("Kemal nerede ? ", femaleUsers.indexOf("Kemal"))  // -1 degeri geldi, yani yok demek. 
 
 // sort
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+console.log("mixItems:", mixItems) 
+console.log("mixItems Sorted:", mixItems.sort())  // eger sort kullanildiysa orijinal Array yeni siralama ile degisir.
+
+function compareNumbers(a, b) {
+    return a - b;
+}
+mixItems.sort(compareNumbers)
+console.log("mixItems Sorted with Function:", mixItems)
 
 // toString
+console.log(maleUsers.toString())  // metin string oldu.
 
 // join
+console.log(
+    items.join(' -> ')
+)
+
+// merge
+// const allUsers = [maleUsers, femaleUsers]
+// const allUsers = femaleUsers.concat(maleUsers)
+// ES6 : femaleUsers.push(...maleUsers)
+// console.log(femaleUsers)
+const allUsers = [...femaleUsers, ...maleUsers]
+console.log(allUsers) 
+
+// const ile array olusturulduysa icine yeni bilgi eklenebilir mi ? 
+allUsers.push("Lorem")
+console.log(allUsers)  
 
 // copy?
+let newUsers = allUsers
+newUsers.push("Ipsum")
+console.log("newUsers", newUsers) 
+console.log("allUsers", allUsers) // Not : yeni liste olusmadi.
+
+newUsers = [...allUsers]
+newUsers.push("Dolor")
+console.log("newUsers", newUsers) 
+console.log("allUsers", allUsers)
+
+newUsers = allUsers.slice()
+newUsers.push("macOs")
+console.log("newUsers", newUsers) 
+console.log("allUsers", allUsers) 
--- 010-JavaScript/js/023-Fonksiyon-ile-Calismak.js
+++ 010-JavaScript/js/023-Fonksiyon-ile-Calismak.js
@@ -1,110 +1,57 @@
 // Fonksiyonlar ile Calismak
-
 // Temel Kurallar:
-// 1: Bir Fonksiyon Bir veya Birden Fazla Parametre Alabilir veya Hic Parametre Almayabilir
-// 2: Bir Fonksiyon Disari Bilgi Gonderebilir(return) veya Gondermeyebilir
 // 3: Mumkunse Fonksiyonun Bagimliliklarini Azaltmak Gerekir
 // 4: Bir Fonksiyon kendisini Tekrar Calistirabilir
 
+// 1: Bir Fonksiyon Bir veya Birden Fazla Parametre Alabilir veya Hic Parametre Almayabilir:
 
-function hello() {// () Hic Parametre Almayabilir
-  console.log("Merhaba Dunya")
+function hello() {  //  "()" Hic parametre almayabilir.
+  console.log("Asdf Qwert")
 }
 
-// Fonksiyonu Calistirmak:
+// Fonskiyonu Calistirmak:
 hello()
 
-// bir veya birden fazla parametreli fonksiyonlar
+// bir veya birden fazla parametreli fonksiyonlar;
 function greetings(name) {
-  console.log(`Merhaba ${name ? name : '' }`)
+  console.log(`Merhaba ${name ? name : 'User' }`) 
 }
+greetings("Cvbnm")  
 
-greetings("Kayra")
 
 function greetings_v2(firstName, lastName) {
-  console.log(`Merhaba ${firstName ? firstName : '' } ${lastName ? lastName : ''}`)
+  console.log(`Merhaba ${firstName ? firstName : 'Firstname' } ${lastName ? lastName : 'Lastname' }`) 
 }
+greetings_v2("Lorem", "Ipsum") 
 
-greetings_v2("Lorem", "Ipsum")
 
 function greetings_v3(firstName = "", lastName = "", age = 18) {
-  console.log("greetings_v3")
-  console.log(`Merhaba ${firstName} ${lastName} -> ${age}`)
+  console.log("greetings_v3;")
+  console.log(`Merhaba ${firstName} ${lastName} -> ${age}`) 
 }
-greetings_v3("Lorem", "Ipsum")
+greetings_v3("Lorem", "Ipsum") 
+
+
+// 2: Bir Fonksiyon Disari Bilgi Gonderebilir(return) veya Gondermeyebilir:
 
 
 function multiply(num1, num2) {
   num1 * num2
-  // disari herhangi bir bilgi gondermese de icer bir islem yapiyor..
-}
-
+  // disari herhangi bir bilgi gondermese de iceride bir islem yapiyor 
+} 
 multiply(10, 20)
 
-function selectHtmlItemAddInformation(element, info) {
+
+function selectHtmlItemAddInformation (element, info) {
   const item = document.querySelector(element)
   item.innerHTML = info
 }
+selectHtmlItemAddInformation('#title', "Hello")
 
-selectHtmlItemAddInformation('#title', "Merhaba Dunya")
-
-// Return Kullanimi
+// Return kullanimi;
 function multiply_with_return(num1, num2) {
-  // fonksiyon icinde bircok sey calisabilir..
-  // const result = num1 * num2
-  // return result
   return num1 * num2
 }
 
 const result = multiply_with_return(10, 30)
-// console.log(result)
-
-// function sayHelloToUser() {
-//   let user;
-//   user = prompt("Adiniz: ")
-//   return user
-// }
-
-// const user = sayHelloToUser()
-// console.log(user)
-
-function sayHelloToLorem() {
-  const user = prompt("Adiniz: ")
-  // if (user === "Lorem") {
-  //   console.log("sayHelloToLorem", user)
-  // } else {
-    //   return sayHelloToLorem()
-    // }
-  if (user !== "Lorem") {
-    return sayHelloToLorem()
-  }
-  console.log("sayHelloToLorem", user)
-}
-
-// sayHelloToLorem()
-
-// Eski Yaptigimiz Bir Calismayi Fonksiyon Olarak Duzenlemek:
-
-function userAccess(userId, userAge) {
-  if (userId.length === 11 && userAge >= 18 && userAge <= 99) {
-    const h1TitleElement = document.querySelector('h1#title')
-    h1TitleElement.innerHTML = '<em>Siteye Girebilirsiniz..</em>'
-    // h1TitleElement.innerText = '<em>Siteye Girebilirsiniz..</em>'
-    // if (userAge > 99) {
-    //   console.log("Yas Bilgisi Hatali..") // acaba burda olmasi dogr muydu ?
-    // }
-    // console.log("Siteye Girebilirsiniz..")
-  } else if (!userAge <= 99) { 
-    console.log("Yas Bilgisi 99dan buyuk olabilir..")
-    // tersini al operatorunu kullandik ;)
-  } else if (userAge > 99) { 
-    console.log("Yas Bilgisi 99dan buyuk olabilir..")
-  } else if (userId.length !== 11) { 
-    console.log("TC No Bilgisi Dogru Olmayabilir..")
-  }
-  else if (userId.length === 11 && userAge < 18) {
-    console.log("Siteye Giremezsiniz Cunku 18 Yasindan Kucuksun..")
-  }
-}
-
-userAccess("Lorem Ipsum", 23)
+console.log(result) 
