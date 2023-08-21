f8b8f47639f7480a76399511163e2deafed7e818
Selim Ocak <selimocakbe@gmail.com>
2023-08-21 16:10:32 +0300

    destructuring

--- 010-JavaScript/js/027-destructuring.js
+++ 010-JavaScript/js/027-destructuring.js
@@ -1,3 +1,4 @@
+
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
 
 // Array:
@@ -7,49 +8,39 @@ console.log(systemInfo[0])
 // const ip = systemInfo[0]
 // const port = systemInfo[1]
 // const dev = systemInfo[2]
-// const username = systemInfo[3]
+// const userName = systemInfo[3]
 // const password = systemInfo[4]
 
 
-// systemInfo Array mi ? Object mi ?
-// const [ip, port, dev, username, password] = systemInfo
-const [ip, port, dev, username, password, ...restInfo] = systemInfo
+// systemInfo Array mi, Object mi ? 
+// const [ip, port, dev, userName, password] = systemInfo 
+const [ip, port, dev, userName, password, ...restInfo] = systemInfo 
 
-console.log(ip, port, dev, username, password, restInfo)
+console.log(ip, port, dev, userName, password, restInfo) 
+
 
 // Object:
 const user = {
   firstName: "Lorem",
-  lastName: "Ipsum Dolor",
+  lastName: "Ipsum",
   age: 29,
-  hobbies: ["dans", "kosu", "bisiklet"],
-  info: ["developer", "freelancer", "javascript"]
-}
+  hobbies: ["spor", "music", "reading"],
+  info: ["freelance", "developer", "javascript"]
+} 
 
-// user Array mi ? Object mi ?
-// const {firstName, lastName, age} = user
+// user Array mi, Object mi ? 
+// const {firstName, lastName, age} = user 
 // -------------------------age: renamedKey
-// const {firstName, lastName, age:userAge} = user
+// const {firstName, lastName, age:userAge} = user 
 
+// const {firstName, lastName, age:userAge, ...rest} = user 
 
-// const {firstName, lastName, age:userAge, ...rest} = user
+// console.log(firstName, lastName, userAge, rest) 
 
-// console.log(firstName, lastName, userAge, rest)
-
-// console.table(user)
+// console.table(user) 
 
 const hello = (obj) => {
-  console.log(obj.firstName, obj.lastName, obj.age)
+  console.log(obj.firstName, obj.lastName, obj.age)  
 }
 
-const helloV2 = ( {firstName, lastName, age} ) => {
-  console.log(firstName, lastName, age)
-}
-
-const helloV3 = ( obj ) => {
-  const {firstName, lastName, age } = obj
-  console.log(firstName, lastName, age)
-}
-
-helloV2(user)
-helloV3(user)
+hello(user) 
