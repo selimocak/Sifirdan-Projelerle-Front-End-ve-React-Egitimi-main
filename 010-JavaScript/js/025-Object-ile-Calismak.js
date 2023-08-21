56ae9f1ad8aa36026b79ef5b6419bfcca8b1ee05
Selim Ocak <selimocakbe@gmail.com>
2023-08-21 14:21:40 +0300

    object

--- 010-JavaScript/js/025-Object-ile-Calismak.js
+++ 010-JavaScript/js/025-Object-ile-Calismak.js
@@ -5,13 +5,14 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/values
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
 
+
 // Obje Olusturmak
 const person = {
   firstName: "Tugra",
-  lastName: "JavaScript",
-  "last login": "2023/01/01",
+  lastName: "Deniz",
+  "last login": "2023/02/02",
   "register-date": "2022/01/01",
-  fullName: function () { // bunu kullan ;)
+  fullName: function () {  // Bunu kullan..
     return `${this.firstName} ${this.lastName}`
   },
   fullNameV2: () => {
@@ -21,42 +22,44 @@ const person = {
 
 // Bilgiye Erismek:
 console.log(person.firstName)
-console.log(person["lastName"])
+console.log(person["lastName"]) 
 console.log(person["last login"])
 console.log(person["register-date"])
 
 // Bilgi Eklemek
-person.age = 22;
+person.age = 18;
 
-person["last-time-changed-password"] = "2023/01/01"
+person["last-time-changed-password"] = "2023/01/01" 
 
 // Bilgi Degistirmek
-person.firstName = "Moca"
-person['lastName'] = "ES6+"
+person.firstName = "Furkan"
+person['lastName'] = "Ersoy"
 
-console.log(person)
+//
+console.log(person) 
 
-console.log(person.fullName())
-console.log(person.fullNameV2())
+console.log(person.fullName()) 
+console.log(person.fullNameV2())  
 
-console.log(Object.keys(person)) // sadece key bilgileri
-console.log(Object.values(person)) // sadece value bilgileri
-console.log(Object.entries(person)) // key ve value bilgileri birlikte array icerisinde
+console.log(Object.keys(person)) 
+console.log(Object.values(person))  
+console.log(Object.entries(person)) 
 
 const product = {
   isInStock: false,
+  name: "",
   price: 0,
   tax: 1.18,
   priceWithTax: function () {
-    return this.price * this.tax
+    return this.price * this.tax 
   }
 }
 
-const iPad = Object.create(product);
+const iPad = Object.create(product)
 iPad.name = "iPad Pro"
 iPad.price = 100;
 iPad.isInStock = true;
 
 
-console.log(iPad)
-console.log(iPad.priceWithTax())
+console.log(iPad)  
+console.log(iPad.priceWithTax())  
