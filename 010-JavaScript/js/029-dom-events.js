5dfe08df73caa018b63c1850f1b7e9115017d11c
Selim Ocak <selimocakbe@gmail.com>
2023-08-22 11:59:12 +0300

    dom-events

--- 010-JavaScript/js/029-dom-events.js
+++ 010-JavaScript/js/029-dom-events.js
@@ -3,43 +3,47 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Element/mouseout_event
 // https://www.javatpoint.com/javascript-events
 
-const btn = document.querySelector('button#hideTitle')
 
-/* 
+const btn = document.querySelector('button#hideTitle') 
+
+/*
+
 // 1. Yontem:
-btn.addEventListener("click", toggleTitle)
 
+btn.addEventListener("click", toggleTitle)
 function toggleTitle() {
-  // console.log('toggle')
   const h1Element = document.querySelector('#title')
-  h1Element.classList.toggle('d-none')
-}
+  h1Element.classList.toggle('d-none') 
+} 
 
 */
 
 /*
-// 2: Yontem: 
-btn.addEventListener("click", function() {
+
+// 2. Yontem
+
+btn.addEventListener("click", function(){
   const h1Element = document.querySelector('#title')
   h1Element.classList.toggle('d-none')
 })
+
 */
 
 btn.addEventListener("click", () => {
   const h1Element = document.querySelector('#title')
-  h1Element.classList.toggle('d-none')
-})
+  h1Element.classList.toggle('d-none') 
+}) 
 
 const h1Element = document.querySelector('#title')
 
 h1Element.addEventListener("mouseenter", () => {
-  console.log("icerde.. ")
+  console.log("icerde.")
   document.body.classList.add("bg-dark", "bg-opacity-50")
   h1Element.classList.toggle("bg-light")
 })
 
 h1Element.addEventListener("mouseout", () => {
-  console.log("disari cikti.. ")
-  document.body.classList.remove("bg-dark", "bg-opacity-50")
-  h1Element.classList.toggle("bg-light")
-})
+  console.log("disarida.")
+  document.body.classList.remove("bg-dark", "bg-opacity-50") 
+  h1Element.classList.toggle("bg-light")  
+}) 
