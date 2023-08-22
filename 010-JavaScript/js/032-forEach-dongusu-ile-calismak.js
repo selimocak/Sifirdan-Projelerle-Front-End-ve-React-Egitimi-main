a52c3b170d9cf15b24c381835e25442b80da0013
Selim Ocak <selimocakbe@gmail.com>
2023-08-22 16:15:21 +0300

    forEach-1

--- 010-JavaScript/js/032-forEach-dongusu-ile-calismak.js
+++ 010-JavaScript/js/032-forEach-dongusu-ile-calismak.js
@@ -9,29 +9,3 @@ const serverInfo = {
   password: "123432453"
 }
 
-users.forEach(item => {
-  console.log(item)
-})
-
-users.forEach(user => {
-  console.log(user)
-})
-
-users.forEach( (item, index) => {
-  console.log(item, index)
-} )
-
-console.log("Array -> ", Object.keys(serverInfo))
-
-Object.keys(serverInfo).forEach(key => {
-  console.log(key)
-  console.log(serverInfo[key])
-})
-
-Object.keys(serverInfo).forEach(key => console.log(serverInfo[key]))
-
-Object.entries(serverInfo).forEach( ([key, value], index) => {
-  console.log(key)
-  console.log(value)
-  console.log(index)
-})
