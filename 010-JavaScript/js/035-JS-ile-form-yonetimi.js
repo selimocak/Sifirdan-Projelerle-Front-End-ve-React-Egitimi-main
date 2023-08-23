f71bbf34c67bbc3654fe60c975a1f80ef4895d87
Selim Ocak <selimocakbe@gmail.com>
2023-08-23 16:08:39 +0300

    js_form-form_input_types

--- 010-JavaScript/js/035-JS-ile-form-yonetimi.js
+++ 010-JavaScript/js/035-JS-ile-form-yonetimi.js
@@ -1,27 +1,28 @@
+
 // https://www.w3schools.com/html/html_form_input_types.asp
 
 const registerFormDom = document.querySelector('#registerForm')
 
-registerFormDom.addEventListener('reset', () => { // bu listener ise isimsiz arrow function ile tanimlandi..
-  console.log('RESETe Basildi')
+registerFormDom.addEventListener('reset', () => {  // bu listener ise, isimsiz arrow function ile tanimlandi.
+  console.log('Resete Basildi.')
 })
 
 registerFormDom.addEventListener('submit', formSubmit)
-// formSubmit arrow function ile kullanilmaya calisinca asagida tanimlandigi icin hakli olarak :) hata aldik.. 
+// formSubmit arrow function ile kullanilmaya calisinca asagida tanimladigi icin hata aldik
 
-/* 
+/*
 Arrow Function :::
 const formSubmit = (event) => {
   event.preventDefault();
-  console.log('Kaydet Butonuna Basildi')
-
-  console.log(event.target)
-}
+  console.log('Kaydet Butonuna Basildi') 
+  console.log(event.target) 
+} 
 */
 
 function formSubmit(event) {
   event.preventDefault();
-  console.log('Kaydet Butonuna Basildi')
+  console.log('Kaydet Butonuna Basildi') 
+  console.log(event.target) 
+} 
 
-  console.log(event.target)
-}
+
