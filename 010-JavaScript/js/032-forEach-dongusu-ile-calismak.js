7dfd44e86a709c7c8cbf9b6705c705cececc817e
Selim Ocak <selimocakbe@gmail.com>
2023-08-23 11:35:32 +0300

    forEach_Dongusu-JS

--- 010-JavaScript/js/032-forEach-dongusu-ile-calismak.js
+++ 010-JavaScript/js/032-forEach-dongusu-ile-calismak.js
@@ -1,3 +1,4 @@
+
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
 
 const users = ["Ayla", "Ahmet", "Mehmet", "Burcin", "Hilal"]
@@ -8,4 +9,32 @@ const serverInfo = {
   user: "admin",
   password: "123432453"
 }
+
+users.forEach(item => {
+  console.log(item) 
+})
+
+users.forEach(user => {
+  console.log(user)  
+})
+
+users.forEach( (item, index) =>  {
+  console.log(item, index) 
+})
+
+console.log("Array -> ", Object.keys(serverInfo)) 
+
+Object.keys(serverInfo).forEach(key => {
+  console.log(key)
+  console.log(serverInfo[key])
+})
+
+Object.keys(serverInfo).forEach(key => console.log(serverInfo[key])) 
+
+Object.entries(serverInfo).forEach( ([key, value], index) => {
+  console.log(key)
+  console.log(value)
+  console.log(index) 
+})
+
 
