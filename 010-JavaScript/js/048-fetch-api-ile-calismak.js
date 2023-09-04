54388fe8373a366bc3564ab0725adabe92a99721
Selim Ocak <selimocakbe@gmail.com>
2023-09-04 16:08:36 +0300

    js-fetch_api

--- 010-JavaScript/js/048-fetch-api-ile-calismak.js
+++ 010-JavaScript/js/048-fetch-api-ile-calismak.js
@@ -1,16 +1,17 @@
+
 // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
 // https://jsonplaceholder.typicode.com/
 
-const JSONPlaceHolderApi = 'https://jsonplaceholder.typicode.com/todos/'
+const JSONPlaceHolderApi = "https://jsonplaceholder.typicode.com/todos/"
 
-// fetch('https://jsonplaceholder.typicode.com/todos/').then(response => {
-//   console.log(response)
-//   return response.json() 
+// fetch('https://jsonplaceholder.typicode.com/todos').then(response => {
+//   console.log(response) 
+//   return response.json()
 // }).then(json => console.log(json))
 
 const app = document.querySelector('#app')
 const createUlElement = document.createElement('ul')
-app.append(createUlElement) 
+app.append(createUlElement)
 
 fetch(JSONPlaceHolderApi).then(res => res.json()).then(json => {
   // json.forEach(item => {
@@ -18,12 +19,12 @@ fetch(JSONPlaceHolderApi).then(res => res.json()).then(json => {
   //   liElement.innerHTML = item.title
   //   createUlElement.append(liElement)
   // })
-})
+}) 
 
 fetch('js/047-json-file.json').then(res => {
   console.log(res)
   return res.json()
-}).then(res => console.log(res)).catch(err => console.error(err))
+}).then(res => console.log(res)).catch(err => console.log(err))
 
 
 async function getTasks(URL) {
@@ -33,7 +34,7 @@ async function getTasks(URL) {
     const liElement = document.createElement('li')
     liElement.innerHTML = item.title
     createUlElement.append(liElement)
-  })
+  })  
 }
 
 getTasks(JSONPlaceHolderApi)
